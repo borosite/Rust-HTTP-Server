6fac2f4327a43c670a056655ae2347417dda69b4
borosite <kakustamanurag@socgen.com>
2022-06-18 22:37:16 +0530

    Added logic to serve HTML files for corresponding GET requests

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,16 @@
 
 use server::HttpServer;
 use website_handler::WebsiteHandler;
+use std::env;
 
 mod server;
 mod http;
 mod website_handler;
 
 fn main() {
+    let default_path = format!("{}\\public", env!("CARGO_MANIFEST_DIR"));    //latter gives the root directory path
+    let public_path = env::var("PUBLIC_PATH").unwrap_or(default_path);
     let server = HttpServer::new("127.0.0.1:8080".to_string());
-    server.run(WebsiteHandler);
+    println!("public path: {}", public_path);
+    server.run(WebsiteHandler::new(public_path));
 }
--- src/website_handler.rs
+++ src/website_handler.rs
@@ -1,14 +1,28 @@
 use super::server::Handler;
 use super::http::{Request, Response, StatusCode, Method};
+use std::fs;
 
-pub struct WebsiteHandler;
+pub struct WebsiteHandler {
+    public_path: String
+}
+
+impl WebsiteHandler {
+    pub fn new(public_path: String) -> Self {
+        Self {public_path}
+    }
+
+    fn read_file(&self, file_path: &str) -> Option<String> {
+        let path = format!("{}/{}", self.public_path, file_path);
+        fs::read_to_string(path).ok()
+    }
+}
 
 impl Handler for WebsiteHandler {
     fn handle_request(&mut self, request: &Request) -> Response {
         match request.method() {
             Method::GET => match request.path() {
-                "/" => Response::new(StatusCode::Ok, Some("<h1>Landing page</h1>".to_string())),
-                "/home" => Response::new(StatusCode::Ok, Some("<h1>Home page</h1>".to_string())),
+                "/" => Response::new(StatusCode::Ok, self.read_file("index.html")),
+                "/home" => Response::new(StatusCode::Ok, self.read_file("home.html")),
                 _ => Response::new(StatusCode::NotFound, None)
             }
             _ => Response::new(StatusCode::NotFound, None)
