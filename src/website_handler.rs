1da64e89ef6eff5fb7707264cc42ad87e48c875f
borosite <kakustamanurag@socgen.com>
2022-06-18 23:07:22 +0530

    Added logic to handle read of other files like CSS or Js. Also added to serve them securely

--- src/website_handler.rs
+++ src/website_handler.rs
@@ -13,7 +13,20 @@ impl WebsiteHandler {
 
     fn read_file(&self, file_path: &str) -> Option<String> {
         let path = format!("{}/{}", self.public_path, file_path);
-        fs::read_to_string(path).ok()
+        println!("path: {}", path);
+        match fs::canonicalize(path) {  //cannot just take in any path that the user provides as that may lead to user getting access to any file on the machine
+            Ok(path) => {
+                if path.starts_with(&self.public_path) {    //doesn't work for windows file path format?*
+                    fs::read_to_string(path).ok()
+                }
+                else {
+                    println!("Directory traversal hack attempted: {}", file_path);
+                    None
+                }
+            },
+            Err(_) => None
+        }
+        
     }
 }
 
@@ -23,7 +36,10 @@ impl Handler for WebsiteHandler {
             Method::GET => match request.path() {
                 "/" => Response::new(StatusCode::Ok, self.read_file("index.html")),
                 "/home" => Response::new(StatusCode::Ok, self.read_file("home.html")),
-                _ => Response::new(StatusCode::NotFound, None)
+                path => match self.read_file(path) {
+                    Some(contents) => Response::new(StatusCode::Ok, Some(contents)),
+                    None => Response::new(StatusCode::NotFound, None)
+                }
             }
             _ => Response::new(StatusCode::NotFound, None)
         }
